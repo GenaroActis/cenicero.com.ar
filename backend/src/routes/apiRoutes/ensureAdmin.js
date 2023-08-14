c4ed3ebb19736cd89c58f1ced65b65342054dbe7
GenaroActis <genaroactis3@gmail.com>
2023-08-14 14:15:43 -0300

    update

--- backend/src/routes/apiRoutes/ensureAdmin.js
+++ backend/src/routes/apiRoutes/ensureAdmin.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { ensureIsAdminController } from '../../controllers/userController.js';
 import { checkAuth } from '../../jwt/auth.js';
+import logger from '../../utils/logger.js'
 const router = Router();
 
+router.get('/loggerTest', (req, res) => {
+    logger.debug('Debug message');
+    logger.http('HTTP message');
+    logger.info('Info message');
+    logger.warning('Warning message');
+    logger.error('Error message');
+    logger.fatal('Fatal message');
+    res.send('Check the logs');
+});
 router.get('/', checkAuth, ensureIsAdminController);
 
 export default router
--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -2,19 +2,37 @@ import winston from 'winston'
 import 'winston-mongodb'
 import { MongoDBUrl } from '../config.js';
 
-export const logger = () =>{
-    const logConfig ={
-        level:'info',
-        transports: [
-            new winston.transports.MongoDB({
-                options:{ useUnifiedTopology: true },
-                db: MongoDBUrl,
-                collection: "logs",
-                tryReconnect: true,
-                level: "error"
-            }),
-            new winston.transports.Console({ level: "silly" })
-        ]
-    };
-    const log = wiston.createLogger(logConfig)
+const logLevels = {
+    debug: 0,
+    http: 1,
+    info: 2,
+    warning: 3,
+    error: 4,
+    fatal: 5
 };
+
+const logConfig ={
+    levels: logLevels,
+    transports: [
+        new winston.transports.MongoDB({
+            options:{ useUnifiedTopology: true },
+            db: MongoDBUrl,
+            collection: "logs",
+            tryReconnect: true,
+            level: "error"
+        }),
+        new winston.transports.Console({ level: "debug" }),
+        new winston.transports.File({
+            filename: "./logs.log",
+            level: "info", 
+        }),
+    ]
+};
+const logger = winston.createLogger(logConfig)
+export default logger;
+// logger.debug('mensaje con nivel debug');
+// logger.http('mensaje con nivel http');
+// logger.info('mensaje con nivel info');
+// logger.warning('mensaje con nivel warning');
+// logger.error('mensaje con nivel error');
+// logger.fatal('mensaje con nivel fatal');
